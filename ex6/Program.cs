a37538f067ac0c667f9eebd288d9b32a15cbe681
angela <nik1955mix@gmail.com>
2022-11-15 00:01:34 +0300

    ex6

---
+++ ex6/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая на вход принимает число и выдает, является ли число чётным (делится ли оно на 2 без остатка)
+// Задача №6
+// Запрос 1 число
+Console.WriteLine("Проверяем является ли число чётным");
+Console.Write("введите число: ");
+int number = int.Parse(Console.ReadLine());
+if (number % 2 == 0)
+{
+    Console.WriteLine($" {number} -> да");
+}
+else
+{
+    Console.WriteLine($" {number} -> нет");
+}
